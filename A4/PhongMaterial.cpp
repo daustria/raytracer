82863a0e90ffb246b0f260a2d6bbcf609bcc4945
Dominic Austria <dgaustri@uwaterloo.ca>
2022-06-07 15:32:59 -0400

    initial commit for raytracer A4

---
+++ A4/PhongMaterial.cpp
@@ -0,0 +1,11 @@
+#include "PhongMaterial.hpp"
+
+PhongMaterial::PhongMaterial(
+	const glm::vec3& kd, const glm::vec3& ks, double shininess )
+	: m_kd(kd)
+	, m_ks(ks)
+	, m_shininess(shininess)
+{}
+
+PhongMaterial::~PhongMaterial()
+{}
---
+++ A4/Primitive.cpp
@@ -0,0 +1,21 @@
+#include "Primitive.hpp"
+
+Primitive::~Primitive()
+{
+}
+
+Sphere::~Sphere()
+{
+}
+
+Cube::~Cube()
+{
+}
+
+NonhierSphere::~NonhierSphere()
+{
+}
+
+NonhierBox::~NonhierBox()
+{
+}
